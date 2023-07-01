ef1ce9a079cb070151c79872795ef03f09abaa20
S10223557-Edison Chong <95247793+seraphinix@users.noreply.github.com>
2023-07-01 10:34:22 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Question5
@@ -25,8 +27,44 @@ public class Question5
      *     2
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
-     
+
     Scanner in = new Scanner(System.in);
-    
+
+    System.out.print("Enter the count of integers: ");
+    int count = in.nextInt();
+
+    int[] numbers = new int[count];
+    System.out.println("Enter the integers:");
+    for (int i = 0; i < count; i++) {
+      numbers[i] = in.nextInt();
+    }
+
+    int mode = calculateMode(numbers);
+    System.out.println("Mode: " + mode);
+
+    in.close();
+  }
+
+  public static int calculateMode(int[] numbers) {
+    Map<Integer, Integer> frequencyMap = new HashMap<>();
+
+    for (int number : numbers) {
+      ((HashMap<?, ?>) frequencyMap).put(number, ((HashMap<?, ?>) frequencyMap).getOrDefault(number, 0) + 1);
+    }
+
+    int mode = 0;
+    int maxFrequency = 0;
+
+    for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+      int number = entry.getKey();
+      int frequency = entry.getValue();
+
+      if (frequency > maxFrequency) {
+        mode = number;
+        maxFrequency = frequency;
+      }
+    }
+
+    return mode;
   }
 }
